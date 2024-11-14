95afe0106e58fb119c8e527b010f4908c6366abe
MA <manish13@gmail.com>
2024-11-13 19:34:00 -0500

    technical factors

--- characteristics.py
+++ characteristics.py
@@ -1,29 +1,34 @@
+import pandas as pd, numpy as np
+import technical_factors as techfac 
+
+
 Technical = {
 'MOM_1M': 'short term reversl 1-1 month',
 'ZEROTRADE': 'number of zero-trading days (3 months)',
 'ME': 'Market Equity',
 'STD_DOL_VOL': 'Std of dollar trading volume (3 motnhs)',
-'MAXRET': 'maximum daily return',
-'ABR': 'abnormal returns around earnings dates',
-'BASPREAD': 'bid-ask spread (3m)',
 'SEAS1A': 'seaonality',
 'BETA': 'Beta (3m)',
 'CHCSHO': 'change in shares outstanding',
 'RVAR_MEAN': 'return variance (3 month)',
 'MOM6M': 'momentum 6 m (2-6 months)',
 'DOLVOL': 'dollar trading volume',
-'ALM': 'asset liquidity',
-'NI': 'net stock issues',
-'ATO': 'asset turnover',
 'MOM60M': 'long term reversal (13-60 months)',
 'MOM36M': 'momemtum 36 m (13-36 month)',
-'RVAR_FF3': 'FF3 residual variance (3 month)',
 'TURN': 'shares turnover',
 'STD_TURN': 'std of share turnover (3 month)',
 'MOM12M': 'momentum (2-13 months)',
 'RVAR_CAPM': 'CAPM residual variance (3 months)',
+'RVAR_FF3': 'FF3 residual variance (3 month)',
+
+'MAXRET': 'maximum daily return',
+'ABR': 'abnormal returns around earnings dates',
+'BASPREAD': 'bid-ask spread (3m)',
+'ALM': 'asset liquidity',
+'NI': 'net stock issues',
 'ILL': 'illiquidity (3 months)',
 'ME_IA': 'industry-adjusted market equity',
+'ATO': 'asset turnover',
 }
 
 Fundamental ={
@@ -62,3 +67,106 @@ Fundamental ={
 'GRLTNOA': '',
 'CHTX': ''
 }
+
+def make_universe():
+    u = techfac.get_universe(techfac.get_price_data())
+    u.to_parquet('data/universe.parquet')
+
+def make_returns():
+    a = techfac.get_price_data()
+    r = techfac.get_data(a.close).pct_change()
+    r[r.abs()>0.2] = np.nan  # this is adhoc cleaning but should suffice for training
+    u = CharacterisitcMaker().universe()
+    r.reindex(columns=u.columns).to_parquet('data/returns.parquet')
+
+def make_price():
+    a = techfac.get_price_data()
+    p = techfac.get_data(a.close).ffill(limit=1)
+    u = CharacterisitcMaker().universe()
+    p.reindex(columns=u.columns).to_parquet('data/price.parquet')
+
+def make_volume():
+    a = techfac.get_price_data()
+    p = techfac.get_data(a.volume).fillna(0)
+    u = CharacterisitcMaker().universe()
+    p.reindex(columns=u.columns).to_parquet('data/volume.parquet')
+
+def make_mktcap():
+    a = techfac.get_price_data()
+    p = techfac.get_data(a.marketCap).ffill(limit=1)
+    u = CharacterisitcMaker().universe()
+    p.reindex(columns=u.columns).to_parquet('data/mktcap.parquet')
+
+def make_ff3():
+    prc = pd.read_parquet('data/price.parquet')
+    a = pd.read_csv('data/FF3.csv', index_col=0, parse_dates=True)
+    a.reindex(index=prc.index).to_parquet('data/ff3.parquet')
+
+
+input_map = {
+    'MOM_1M': ['returns'],
+    'ZEROTRADE': ['volume'],
+    'ME': ['mktcap'],
+    'STD_DOL_VOL': ['volume', 'price'],
+    'SEAS1A': ['returns'],
+    'BETA': ['returns', 'market'],
+    'CHCSHO': ['mktcap', 'price'],
+    'RVAR_MEAN': ['returns'],
+    'MOM6M': ['returns'],
+    'DOLVOL': ['volume', 'price'],
+    'MOM60M': ['returns'],
+    'MOM36M': ['returns'],
+    'TURN': ['volume', 'mktcap', 'price'],
+    'STD_TURN': ['volume', 'mktcap', 'price'],
+    'MOM12M': ['returns'],
+    'RVAR_CAPM': ['returns', 'market'],
+    'RVAR_FF3': ['returns', 'ff3'],
+}
+class CharacterisitcMaker:
+    def __init__(self, name: str = None):
+        assert name in input_map
+        self.name = name # name of the characteristic
+
+    def universe(self):
+        return pd.read_parquet('data/universe.parquet')
+
+    def returns(self):
+        return pd.read_parquet('data/returns.parquet')
+    
+    def price(self):
+        return pd.read_parquet('data/price.parquet')
+    
+    def volume(self):
+        return pd.read_parquet('data/volume.parquet')
+    
+    def mktcap(self):
+        return pd.read_parquet('data/mktcap.parquet')
+    
+    def ff3(self):
+        return pd.read_parquet('data/ff3.parquet')[['Mkt-RF','SMB','HML']]
+    
+    def market(self):
+        ff3 = pd.read_parquet('data/ff3.parquet')
+        return ff3['Mkt-RF'] + ff3['RF']
+    
+    def get_params(self):
+        # for self.name return all parameters required
+        return [getattr(self, i)() for i in input_map[self.name]]
+
+    def __call__(self, save=False):
+        if self.name in Technical:
+            feature = techfac.shapify(getattr(techfac, self.name)(*self.get_params()), self.universe())
+            if save:
+                feature.to_parquet(f'/data/{self.name}.parquet')
+            return feature
+        else:
+            raise NotImplementedError('Fundamental factors not implemented yet!')
+        
+
+if __name__ == '__main__':
+    # make_universe()
+    make_ff3()
+    # df = CharacterisitcMaker(name='MOM_1M')()
+    
+    
+
--- technical_factors.py
+++ technical_factors.py
@@ -2,20 +2,117 @@ import pandas as pd
 import numpy as np
 # construct the technical factors using price_volume_data
 
-root = '/home/manish/USData/data/us_stocks.parquet'
+# data read utils
+def get_price_data():
+    root = '/home/manish/USData/data/us_stocks.parquet'
+    a = pd.read_parquet(root)
+    return a
 
-a = pd.read_parquet(root)
+def get_ff3():
+    root = '/home/manish/USData/FF3.csv'
+    a = pd.read_csv(root, index_col=0, parse_dates=True)    
+    return a
 
-# construct the universe
-mcap = a.marketCap
-mcap=mcap[~mcap.index.duplicated(keep='last')].unstack(level=0)
-mr = mcap.rolling(63, min_periods=10).mean().rank(axis=1, ascending=False)
-u = mr<1000
-import datetime as dt
-u=u[u.index>'19920401']
-u=u[u.columns[u.sum()>0]]
-print(u.shape)
+# universe construction
+def get_universe(a):
+    mcap = a.marketCap
+    mcap=mcap[~mcap.index.duplicated(keep='last')].unstack(level=0)
+    mr = mcap.rolling(63, min_periods=10).mean().rank(axis=1, ascending=False)
+    u = mr<1000
+    import datetime as dt
+    u=u[u.index>'19920401']
+    u=u[u.columns[u.sum()>0]]
+    return u 
+
+# data utils 
+def get_mkt(ret, mcap):
+    return (ret * np.sqrt(mcap)).sum(1).divide(np.sqrt(mcap).sum(1), axis=0)
+
+def get_shares(mcap, prc):
+    return mcap/prc 
+
+def get_dolvol(vol, prc):
+    return vol * prc
+
+def get_data(df):
+    return df[~df.index.duplicated(keep='last')].unstack(level=0)
+
+def shapify(df, u):
+    return df.reindex(columns=u.columns, index=u.index)[u]
 
 
-# get return/volume/mcap data
+# technical factors
+def MOM_1M(ret):
+    return -ret.rolling(21, min_periods=1).mean()
+
+def ZEROTRADE(vol):
+    return (vol==0).rolling(21*3, min_periods=1).sum()
+
+def ME(mcap):
+    return mcap 
+
+def STD_DOL_VOL(vol, prc):
+    return (vol*prc).rolling(21*3, min_periods=5).std()
+
+def SEAS1A(ret):
+    return ret.rolling(21, min_periods=5).mean().shift(252-10)
+
+def BETA(ret, mkt):
+    return ret.rolling(21*3, min_periods=5).cov(mkt)/mkt.rolling(21*3, min_periods=5).var()
+
+def CHCSHO(mcap, prc):
+    return (mcap/prc).diff(1)
+
+def RVAR_MEAN(ret):
+    return ret.rolling(21*3, min_periods=5).var()
+
+def MOM6M(ret):
+    return ret.rolling(21*5, min_periods=5).mean().shift(21)
+
+def DOLVOL(vol, prc):
+    return vol*prc 
+
+def MOM60M(ret):
+    return - ret.rolling(21*60, min_periods=21*12).shift(12*21)
+
+def MOM36M(ret):
+    return ret.rolling(21*36, min_periods=21*12).shift(12*21)
+
+def TURN(vol, mcap, prc):
+    return vol/mcap * prc
+
+def STD_TURN(vol, mcap, prc):
+    return (vol/mcap*prc).rolling(21*3, min_periods=5).std()
+
+def MOM12M(ret):
+    return ret.rolling(21*12, min_periods=21).shift(21)
+
+def RVAR_CAPM(ret, mkt):
+    beta = BETA(ret, mkt)
+    e = ret - beta * mkt 
+    return e.rolling(12*21, min_periods=21).std()
+
+def RVAR_FF3(ret, ff3):
+    beta = pd.DataFrame({BETA(ret, ff3[i]) for i in ff3.columns})
+    e = ret - beta * ff3 
+    return e.rolling(12*21, min_periods=21).std()
+
+if __name__ == '__main__':
+    # get price data
+    a = get_price_data()
+    print(a.shape, a.columns)
+
+    # construct universe
+    u = get_universe(a)
+    print(u.shape)
+
+    # get return/volume/mcap data
+    r = get_data(a.changePercent, u)
+    print(r.shape)
+    m = get_data(a.marketCap, u)
+    print(m.shape)
+    v = get_data(a.volume, u)
+    print(v.shape)
+    c = get_data(a.close, u)
+    print(c.shape)
 
