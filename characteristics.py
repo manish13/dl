ec2aec838e83464b94d0cca9032da30d3dd236fb
MA <manish13@gmail.com>
2024-11-14 22:37:13 -0500

    minor

--- characteristics.py
+++ characteristics.py
@@ -1,4 +1,4 @@
-import pandas as pd, numpy as np
+aimport pandas as pd, numpy as np
 import technical_factors as techfac 
 
 
@@ -68,39 +68,41 @@ Fundamental ={
 'CHTX': ''
 }
 
+ROOT = '/home/manish/code/dl/data/'
+
 def make_universe():
     u = techfac.get_universe(techfac.get_price_data())
-    u.to_parquet('data/universe.parquet')
+    u.to_parquet(f'{ROOT}universe.parquet')
 
 def make_returns():
     a = techfac.get_price_data()
     r = techfac.get_data(a.close).pct_change()
     r[r.abs()>0.2] = np.nan  # this is adhoc cleaning but should suffice for training
     u = CharacterisitcMaker().universe()
-    r.reindex(columns=u.columns).to_parquet('data/returns.parquet')
+    r.reindex(columns=u.columns).to_parquet(f'{ROOT}eturns.parquet')
 
 def make_price():
     a = techfac.get_price_data()
     p = techfac.get_data(a.close).ffill(limit=1)
     u = CharacterisitcMaker().universe()
-    p.reindex(columns=u.columns).to_parquet('data/price.parquet')
+    p.reindex(columns=u.columns).to_parquet(f'{ROOT}price.parquet')
 
 def make_volume():
     a = techfac.get_price_data()
     p = techfac.get_data(a.volume).fillna(0)
     u = CharacterisitcMaker().universe()
-    p.reindex(columns=u.columns).to_parquet('data/volume.parquet')
+    p.reindex(columns=u.columns).to_parquet(f'{ROOT}volume.parquet')
 
 def make_mktcap():
     a = techfac.get_price_data()
     p = techfac.get_data(a.marketCap).ffill(limit=1)
     u = CharacterisitcMaker().universe()
-    p.reindex(columns=u.columns).to_parquet('data/mktcap.parquet')
+    p.reindex(columns=u.columns).to_parquet(f'{ROOT}mktcap.parquet')
 
 def make_ff3():
-    prc = pd.read_parquet('data/price.parquet')
-    a = pd.read_csv('data/FF3.csv', index_col=0, parse_dates=True)
-    a.reindex(index=prc.index).to_parquet('data/ff3.parquet')
+    prc = pd.read_parquet(f'{ROOT}price.parquet')
+    a = pd.read_csv(f'{ROOT}FF3.csv', index_col=0, parse_dates=True)
+    a.reindex(index=prc.index).to_parquet(f'{ROOT}ff3.parquet')
 
 
 input_map = {
@@ -128,25 +130,25 @@ class CharacterisitcMaker:
         self.name = name # name of the characteristic
 
     def universe(self):
-        return pd.read_parquet('data/universe.parquet')
+        return pd.read_parquet(f'{ROOT}universe.parquet')
 
     def returns(self):
-        return pd.read_parquet('data/returns.parquet')
+        return pd.read_parquet(f'{ROOT}returns.parquet')
     
     def price(self):
-        return pd.read_parquet('data/price.parquet')
+        return pd.read_parquet(f'{ROOT}price.parquet')
     
     def volume(self):
-        return pd.read_parquet('data/volume.parquet')
+        return pd.read_parquet(f'{ROOT}volume.parquet')
     
     def mktcap(self):
-        return pd.read_parquet('data/mktcap.parquet')
+        return pd.read_parquet(f'{ROOT}mktcap.parquet')
     
     def ff3(self):
-        return pd.read_parquet('data/ff3.parquet')[['Mkt-RF','SMB','HML']]
+        return pd.read_parquet(f'{ROOT}ff3.parquet')[['Mkt-RF','SMB','HML']]
     
     def market(self):
-        ff3 = pd.read_parquet('data/ff3.parquet')
+        ff3 = pd.read_parquet(f'{ROOT}ff3.parquet')
         return ff3['Mkt-RF'] + ff3['RF']
     
     def get_params(self):
@@ -155,9 +157,9 @@ class CharacterisitcMaker:
 
     def __call__(self, save=False):
         if self.name in Technical:
-            feature = techfac.shapify(getattr(techfac, self.name)(*self.get_params()), self.universe())
+            feature = techfac.shapify(getattr(techfac, self.name)(*self.get_params()).ffill(), self.universe())
             if save:
-                feature.to_parquet(f'/data/{self.name}.parquet')
+                feature.resample('M').last().to_parquet(f'{ROOT}{self.name}.parquet')
             return feature
         else:
             raise NotImplementedError('Fundamental factors not implemented yet!')
@@ -165,8 +167,13 @@ class CharacterisitcMaker:
 
 if __name__ == '__main__':
     # make_universe()
-    make_ff3()
-    # df = CharacterisitcMaker(name='MOM_1M')()
+    # make_ff3()
+    for i in input_map:
+        print('generating', i)
+        try:
+            df = CharacterisitcMaker(name=i)(True)
+        except:
+            print(i)
     
     
 
